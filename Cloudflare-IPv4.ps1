ef1a4e794d8dbcdc6f1a66062a41c9b91c4eeca7
kimboslice99 <94807745+kimboslice99@users.noreply.github.com>
2022-07-05 18:31:47 -0400

    Create Cloudflare-IPv4.ps1

---
+++ Cloudflare-IPv4.ps1
@@ -0,0 +1,19 @@
+# Fill in your CF email, API key, ZoneID & Record details
+$email="Email_Address"
+$apikey="API_KEY"
+$ZoneID = "Zone_ID"
+$type = "A"
+$recordname = "sub.domain.tld"
+$ttl = "1" # Must be between 60 and 86400, or 1 for 'automatic'
+
+$CurrentIP=Invoke-RestMethod -Uri "https://ipecho.net/plain'"
+$result = Invoke-RestMethod -Uri "https://api.cloudflare.com/client/v4/zones/$ZoneID/dns_records?type=$type&name=$recordname&page=1&per_page=100&order=type&direction=desc&match=all" -Method 'GET' -ContentType "application/json" -Headers @{'Accept'='application/json';'X-Auth-Email'="$email";'X-Auth-Key'="$apikey"} |
+           % {$_.result}
+$RecordID = ($result).id
+$IP = ($result).content
+$prox = ($result).proxied
+$proxied = "$prox".ToLower()  # Cloudflare API outputs "False"/"True" from above iwr but will not accept it in the next iwr
+If ($CurrentIP -eq $ip) { Write-Host "IP Same!"
+                          Exit }
+                    Else { Write-Host "IP Changed! $CurrentIP doesnt equal $ip"}
+Invoke-WebRequest -Uri "https://api.cloudflare.com/client/v4/zones/$ZoneID/dns_records/$RecordID" -Method 'PUT' -Body "{`"type`":`"$type`",`"name`":`"$recordname`",`"content`":`"$CurrentIP`",`"ttl`":$ttl,`"proxied`":$proxied}" -ContentType "application/json" -Headers @{'Accept'='application/json';'X-Auth-Email'="$email";'X-Auth-Key'="$apikey"}
