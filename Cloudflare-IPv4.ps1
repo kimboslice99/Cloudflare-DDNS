93a9cbe00c6fffcc4c70a1d4bbd3dbc03f4eafa4
kimboslice99 <94807745+kimboslice99@users.noreply.github.com>
2022-07-05 19:02:47 -0400

    Update Cloudflare-IPv4.ps1

--- Cloudflare-IPv4.ps1
+++ Cloudflare-IPv4.ps1
@@ -7,7 +7,7 @@ $recordname = "sub.domain.tld"
 $ttl = "1" # Must be between 60 and 86400, or 1 for 'automatic'
 
 Try { $CurrentIP=Invoke-RestMethod -Uri "https://ipecho.net/plain" }
-     Catch { No connection!
+     Catch { Write-Host "No connection!"
 	  Exit }
 Try { $result = Invoke-RestMethod -Uri "https://api.cloudflare.com/client/v4/zones/$ZoneID/dns_records?type=$type&name=$recordname&page=1&per_page=100&order=type&direction=desc&match=all" -Method 'GET' -ContentType "application/json" -Headers @{'Accept'='application/json';'X-Auth-Email'="$email";'X-Auth-Key'="$apikey"} |
                % {$_.result} }
